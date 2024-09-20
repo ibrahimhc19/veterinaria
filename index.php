2fabaabb528a6ba11c4c53d46b8237edbb98b3d8
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2024-09-20 13:22:50 -0500

    bibliotecas

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Registrar Mascotas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./public/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./public/css/sweetalert.css">
     <link href="./public/css/datatables.min.css" rel="stylesheet">
     <link href="./public/css/styles.css" rel="stylesheet">
   </head>
@@ -50,6 +52,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./public/js/datatables.min.js"></script>
+    <script src="./public/js/sweetalert.min.js"></script>
     <script src="./public/js/jquery.js"></script>
     <script src="./public/js/funciones.js"></script>
   </body>
